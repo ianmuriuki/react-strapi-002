d19cdd483b5ceee2157b26f528e1994e96f85d27
ian_muriuki <ianmuriuki.inc@gmail.com>
2025-02-05 17:00:35 +0300

    changes on middlewares.ts

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,12 +1,12 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  'strapi::logger', // Built-in logger
+  'strapi::errors', // Built-in error handler
+  'strapi::security', // Secure headers
+  'strapi::cors', // Cross-Origin Resource Sharing (CORS) headers
+  'strapi::poweredBy',  // Add headers related to powered by strapi
+  'strapi::query', // Parse URL params
+  'strapi::body', // Parse HTTP body
+  'strapi::session', // Session middleware
+  'strapi::favicon',  // Add favicon
+  'strapi::public', // Serve static files
 ];
