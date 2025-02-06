01291b4a0730349ccac9d6fdb7a47ed5b3b9c70a
ian_muriuki <ianmuriuki.inc@gmail.com>
2025-02-06 17:13:32 +0300

    minor updates

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -2,7 +2,16 @@ export default [
   'strapi::logger', // Built-in logger
   'strapi::errors', // Built-in error handler
   'strapi::security', // Secure headers
-  'strapi::cors', // Cross-Origin Resource Sharing (CORS) headers
+  // Cross-Origin Resource Sharing (CORS) headers
+  {
+    name: 'strapi::cors', // Cross-Origin Resource Sharing (CORS) headers
+    config: {
+      origin: ['http://localhost:5173', 'https://yourfrontendurl.com'], // Add your frontend URLs here
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      credentials: true, // Enable sending cookies with requests (if needed)
+    }
+  },
   'strapi::poweredBy',  // Add headers related to powered by strapi
   'strapi::query', // Parse URL params
   'strapi::body', // Parse HTTP body
